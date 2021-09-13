2f13f1e252b8129ada677170f8e61b7def093d7a
NicholasNeto <nicholasneto@outlook.com>
2021-09-12 22:35:46 -0300

    implements interceptors of axios  in project

--- service/api.ts
+++ service/api.ts
@@ -1,6 +1,17 @@
-import axios from 'axios';
-import { parseCookies } from 'nookies'
-const cookies = parseCookies()
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies'
+
+
+{/*
+    Aqui é uma funcionalidade para interceptar uma chamada, então imagina que 
+    vocẽ quer redirecionar todos os erros 403 para login você poderia criar um 
+    interceptador destes no axios, existe interceptador de request (indo para servidor)
+    e resposta ao voltar do servidor ex:  interceptors.response || interceptors.request
+*/}
+
+
+let cookies = parseCookies()
+
 
 export const api = axios.create({
     baseURL: 'http://localhost:3333',
@@ -8,3 +19,37 @@ export const api = axios.create({
         Authorization: `Bearer ${cookies['nextauth.token']}`
     }
 })
+
+
+api.interceptors.response.use(response => {
+    return response;
+}, (error: AxiosError) => {
+    // error.response?.status // StatusCode ex: 401 | 403 etc ... 
+    if (error.response?.status === 401) {
+        if (error.response?.data.code === 'token.expired') {
+            // renovar o token 
+            cookies = parseCookies()
+            const { 'nextauth.refreshToken': refreshToken } = cookies
+
+            api.post('/refresh', {
+                refreshToken,
+            }).then(response => {
+                const { token } = response.data
+                setCookie(undefined, 'nextauth.token', token, {
+                    maxAge: 60 * 60 * 24 * 30, // 30 days
+                    path: '/'
+                })
+
+                setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+                    maxAge: 60 * 60 * 24 * 30, // 30 days
+                    path: '/'
+                })
+
+                api.defaults.headers['Authorization'] = `Bearer ${token}`
+            });
+        } else {
+            // desligar o usuario  
+        }
+    }
+})
+
