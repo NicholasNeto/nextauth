44fc4b74a859292d1a7a99c51feb458771e2c444
NicholasNeto <nicholasneto@outlook.com>
2021-09-13 00:18:23 -0300

    add catch for request /me

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,16 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { api } from "../service/api"
 
 export default function Dashboard() {
 
     const { user } = useContext(AuthContext)
+
+    useEffect(() => {
+        api.get('/me')
+        .then(response => console.log(response))
+        .catch(error => console.error(error))
+    }, [])
 
     return (
         <h1>Dashboard {user?.email}</h1>
