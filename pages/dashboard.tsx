1555e71a82682c2cff4a5e4892aa9cf76f27a8e9
NicholasNeto <nicholasneto@outlook.com>
2021-09-25 10:33:20 -0300

    used hook useCan

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../service/api"
 import { api } from "../service/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
@@ -8,14 +9,25 @@ export default function Dashboard() {
 
     const { user } = useContext(AuthContext)
 
+    const userCanSeeMetrics = useCan({
+        permissions: ['metrics.list']
+        // roles : poderia ser roles:  [administrador, editor]  os dois podem
+    })
+
     useEffect(() => {
         api.get('/me')
-        .then(response => console.log(response))
-        .catch(error => console.error(error))
+            .then(response => console.log(response))
+            .catch(error => console.error(error))
     }, [])
 
     return (
-        <h1>Dashboard {user?.email}</h1>
+        <>
+            <h1>Dashboard {user?.email}</h1>
+            {userCanSeeMetrics && <div>
+                Metrics
+            </div>}
+        </>
+
     )
 }
 
@@ -26,6 +38,6 @@ export const getServerSideProps = withSSRAuth(async (ctx) => {
     console.log(response)
 
     return {
-      props: {}
+        props: {}
     }
-  })
+})
